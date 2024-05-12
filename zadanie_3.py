9410a441d592c0ac0eea64ddfe4390773e8eeda4
254323 <236094@edu.p.lodz.pl>
2024-05-12 23:50:54 +0200

    //

--- zadanie_3.py
+++ zadanie_3.py
@@ -19,13 +19,11 @@ def extract_texture_samples(input_dir, output_dir, sample_size):
                 image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
                 height, width = image.shape
 
-                category = os.path.basename(root)  # Kategoria tekstury to nazwa bieżącego katalogu
+                category = os.path.basename(root)
 
-                # Utwórz katalog docelowy, jeśli nie istnieje
                 output_category_dir = os.path.join(output_dir, category)
                 os.makedirs(output_category_dir, exist_ok=True)
 
-                # Wycinaj próbki z obrazu i zapisuj do odpowiedniego katalogu
                 for i in range(0, height - sample_size[0], sample_size[0]):
                     for j in range(0, width - sample_size[1], sample_size[1]):
                         sample = image[i:i+sample_size[0], j:j+sample_size[1]]
@@ -40,13 +38,10 @@ def calculate_texture_features(input_dir, distances=[1, 3, 5], angles=[0, np.pi 
                 image_path = os.path.join(root, file)
                 image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-                # Oblicz macierz zdarzeń szarości dla danego obrazu
                 glcm = mhtex.haralick(image.astype(np.uint8))
 
-                # Utwórz słownik cech dla każdej próbki
                 features = {'file': file, 'category': os.path.basename(root).split('_')[0]}  # Użyj części nazwy katalogu jako kategorii
 
-                # Oblicz wybrane cechy tekstury dla różnych odległości i kątów
                 for prop in ['dissimilarity', 'correlation', 'contrast', 'energy', 'homogeneity', 'ASM']:
                     for dist in distances:
                         for ang in angles:
@@ -57,7 +52,7 @@ def calculate_texture_features(input_dir, distances=[1, 3, 5], angles=[0, np.pi 
                 features_list.append(features)
 
     features_df = pd.DataFrame(features_list)
-    print(f"Wyekstrahowano cechy dla {len(features_df)} próbek.")
+    print(f"Przetworzono {len(features_df)} próbek.")
     return features_df
 
 
@@ -69,13 +64,11 @@ def save_features_to_csv(features, output_file):
 def classify_features(features_file, test_size=0.2):
     df = pd.read_csv(features_file)
 
-    # Usuń atrybut nieliczbowy 'file'
     df = df.drop('file', axis=1)
 
     X = df.drop('category', axis=1)
     y = df['category']
 
-    # Przekształć etykiety na liczby całkowite
     label_encoder = LabelEncoder()
     y = label_encoder.fit_transform(y)
 
@@ -85,14 +78,12 @@ def classify_features(features_file, test_size=0.2):
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
 
-    # Przekształć przewidziane etykiety z powrotem na oryginalne etykiety
     y_test = label_encoder.inverse_transform(y_test)
     y_pred = label_encoder.inverse_transform(y_pred)
 
     accuracy = accuracy_score(y_test, y_pred)
     print("Dokładność klasyfikatora:", accuracy)
 
-# Ścieżki do katalogów
 input_dir = r'C:\Users\Dell T3500\Desktop\struktury'  # Katalog z oryginalnymi obrazami
 output_dir = r'C:\Users\Dell T3500\Desktop\wycinki'  # Katalog, w którym zostaną zapisane wycinki
 features_file = 'texture_features.csv'
@@ -100,9 +91,7 @@ features_file = 'texture_features.csv'
 sample_size = (128, 128)
 extract_texture_samples(input_dir, output_dir, sample_size)
 
-# Wyznaczanie cech tekstury
 texture_features = calculate_texture_features(output_dir)
 save_features_to_csv(texture_features, features_file)
 
-# Klasyfikacja cech tekstury
 classify_features(features_file)
